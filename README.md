3107c8cf3d6105138f2c75d29600a616ba4c70af
Thien Tran <gau.nernst@yahoo.com.sg>
2023-10-21 11:01:07 +0700

    Update Makefile. Include GLAD (#2)

--- README.md
+++ README.md
@@ -2,14 +2,9 @@
 
 https://learnopengl.com/
 
-Instructions below are for macOS.
-
-Add GLAD to project (https://glad.dav1d.de/)
-- Select API gl = Version 3.3, Profile = Core. Then generate
-- Copy `include/glab` and `include/KHR` to `include`. Copy `src/glad.c` to `src`.
-
-Compile and link
+Unix
 
 ```bash
-clang src/*.c -o main -Iinclude -Llib -lglfw3 -lglad -framework Cocoa -framework OpenGL -framework IOKit
+make glfw
+make launch
 ```
--- src/main.c
+++ src/main.c
@@ -1,4 +1,45 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static unsigned int CompileShader(unsigned int type, const char *source) {
+  unsigned int id = glCreateShader(type);
+  glShaderSource(id, 1, &source, NULL);
+  glCompileShader(id);
+
+  int result;
+  glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+  if (result == GL_FALSE) {
+    int length;
+    glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+    char *message = malloc(length * sizeof(char));
+    glGetShaderInfoLog(id, length, &length, message);
+    fprintf(stderr, "Failed to compile shader\n");
+    fprintf(stderr, "%s\n", message);
+    glDeleteShader(id);
+    free(message);
+    return 0;
+  }
+
+  return id;
+}
+
+static unsigned int CreateShader(const char *vertexShader, const char *fragmentShader) {
+  unsigned int program = glCreateProgram();
+  unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+  unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+  glAttachShader(program, vs);
+  glAttachShader(program, fs);
+  glLinkProgram(program);
+  glValidateProgram(program);
+
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+
+  return program;
+}
 
 int main(void) {
   GLFWwindow *window;
@@ -6,6 +47,13 @@ int main(void) {
   /* Initialize the library */
   if (!glfwInit())
     return -1;
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // for macOS
+#endif
 
   /* Create a windowed mode window and its OpenGL context */
   window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -17,10 +65,49 @@ int main(void) {
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
 
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    return 1;
+  }
+
+  float positions[] = {
+      -0.5f, -0.5f, //
+      0.0f,  0.0f,  //
+      0.5f,  -0.5f,
+  };
+
+  unsigned int buffer, vao;
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+  // using index=0
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
+
+  char *vertexShader = "#version 330 core\n"
+                       "\n"
+                       "layout(location=0) in vec4 position;" // index of the attribute = 0
+                       "void main() {\n"
+                       "  gl_Position = position;\n"
+                       "}\n";
+  char *fragmentShader = "#version 330 core\n"
+                         "\n"
+                         "layout(location=0) out vec4 color;" // index of the attribute = 0
+                         "void main() {\n"
+                         "  color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+                         "}\n";
+  unsigned int shader = CreateShader(vertexShader, fragmentShader);
+  glUseProgram(shader);
+
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+      glfwSetWindowShouldClose(window, 1);
+
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
